c67d4dad4f11375e8daebc4bd66dbe0e596e0de8
Semenov Petr <under_karter49@icloud.com>
2022-07-11 22:33:44 +0300

    Add UITableView and StaticViewCell.

--- MVP Setting Screen/Contollers/ViewController.swift
+++ MVP Setting Screen/Contollers/ViewController.swift
@@ -8,15 +8,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let tableView: UITableView = {
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(
+            StaticViewCell.self,
+            forCellReuseIdentifier: StaticViewCell.identifier
+        )
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
         navigationController?.navigationBar.prefersLargeTitles = true
         title = "Настройки"
+        
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame = view.bounds
+        
+        configure()
     }
-
-
+    
+    func configure() {
+        Data.models = Array(0...10).compactMap({
+                StaticOption(title: "Line \($0)",
+                             icon: UIImage(systemName: "bell"),
+                             iconBackgroundColor: UIColor.systemRed){
+                }
+            })
+        }
+    
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = Data.models[indexPath.row]
+        
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: StaticViewCell.identifier,
+            for: indexPath) as? StaticViewCell else {
+                return UITableViewCell()
+            }
+        cell.configure(with: model)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Data.models.count
+    }
+}
---
+++ MVP Setting Screen/Models/Models.swift
@@ -0,0 +1,25 @@
+//
+//  Models.swift
+//  MVP Setting Screen
+//
+//  Created by temp user on 11.07.2022.
+//
+
+import Foundation
+import UIKit
+
+struct StaticOption {
+    let title: String
+    let icon: UIImage?
+    let iconBackgroundColor: UIColor
+    let handler: (()->Void)
+}
+
+struct Data {
+    var models = [StaticOption]()
+}
+
+struct Metrics {
+    
+}
+
